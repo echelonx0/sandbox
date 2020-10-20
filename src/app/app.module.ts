0464e56348128dbfb05b88e33f614b752cb2ebfc
Seigneur Consults <ig.ultrabix@gmail.com>
2020-10-20 18:54:46 +0200

    latest changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CountsComponent } from './components/counts/counts.component';
+
+import { environment } from '../environments/environment';
+
+
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
+// 1. firebase related imports
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    CountsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,   
+    FlashMessagesModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule,
+     // 3. Initialize
+     AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
